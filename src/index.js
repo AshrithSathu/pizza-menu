d230cc07b4c413b3e2dfe36f9d7a0fead195a8a3
AshrithSathu <sathuashrith@gmail.com>
2023-12-31 21:02:58 +0530

    adding whole project from local machine

---
+++ src/index.js
@@ -0,0 +1,143 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
+
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+
+function App() {
+
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>
+                Fast React Pizza Co.
+            </h1>
+        </header>
+
+    )
+}
+
+function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+            {numPizzas > 0 ?
+                <ul className="pizzas">
+                    {pizzas.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul> : null}
+            {/* <Pizza
+                pizzaName="Pizza spinaci"
+                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={10}
+            />
+            <Pizza
+                pizzaName="Pizza Funghi"
+                ingredients="Tomato, mozarella, mushrooms, and onion"
+                price={12}
+                photoName="pizzas/funghi.jpg"
+            /> */}
+        </main>
+    )
+}
+
+function Pizza({ pizzaObj }) {
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : " "}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                {/* <span>{pizzaObj.price}</span> */}
+                {pizzaObj.soldOut ? "SOLD OUT!" : pizzaObj.price}
+            </div>
+
+        </li >
+
+    )
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closedHour = 22;
+    const isOpen = hour >= openHour && hour <= closedHour;
+
+    return (
+        <footer className="footer">
+            {isOpen ?
+                <div className="order">
+                    <p> We're Open until {closedHour}
+                        :00. Come visit us or order online.
+
+                    </p>
+                    <button className="btn" >Order</button>
+                </div> : <p>We're happy to welcome you between {openHour}:00 and {closedHour}:00 </p>
+            }
+        </footer>)
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
